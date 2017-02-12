756d1004cdc163dcaa274eed70b9df43001317d4
katemanson <katemanson107@gmail.com>
2017-02-12 15:27:32 +0000

    adds .getQuestionAthletes() and .orderAthletesByPoints() to Game class

--- app/src/main/java/com/example/kate/playerrank/Game.java
+++ app/src/main/java/com/example/kate/playerrank/Game.java
@@ -3,6 +3,8 @@ package com.example.kate.playerrank;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 /**
  * Created by admin on 12/02/2017.
@@ -20,6 +22,36 @@ public class Game {
 
     public ArrayList<Athlete> getCopyAthletes() {
         return this.athletes.getCopyList();
+    }
+
+    public int getRandomAthleteIndex(ArrayList<Athlete> athleteList) {
+        Random randomThing = new Random();
+        int listSize = athleteList.size();
+        int index = randomThing.nextInt(listSize);
+        return index;
+    }
+
+    public Athlete removeRandomAthlete(ArrayList<Athlete> athleteList) {
+        return athleteList.remove(getRandomAthleteIndex(athleteList));
+    }
+
+    public ArrayList<Athlete> getQuestionAthletes(int numberOfAthletes) {
+        ArrayList<Athlete> questionAthletes = new ArrayList<>();
+        ArrayList<Athlete> athletePool = getCopyAthletes();
+
+        for (int i = 0; i < numberOfAthletes; i++) {
+            Athlete questionAthlete = removeRandomAthlete(athletePool);
+            questionAthletes.add(questionAthlete);
+            System.out.println("questionAthletes: " + questionAthletes.size());
+            System.out.println("athletePool: " + athletePool.size());
+        }
+
+        return questionAthletes;
+    }
+
+    public ArrayList<Athlete> orderAthletesByPoints(ArrayList<Athlete> athleteList) {
+        Collections.sort(athleteList);
+        return athleteList;
     }
 
 
--- app/src/test/java/com/example/kate/playerrank/GameTest.java
+++ app/src/test/java/com/example/kate/playerrank/GameTest.java
@@ -2,6 +2,9 @@ package com.example.kate.playerrank;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -11,14 +14,39 @@ import static org.junit.Assert.*;
 public class GameTest {
 
     Game testGame;
+    Athlete testAthleteOne;
+    Athlete testAthleteTwo;
+    Athlete testAthleteThree;
+    ArrayList<Athlete> testList;
 
     @Before
     public void before() {
         testGame = new Game();
+        testAthleteOne = new Athlete(1, "Stephen", "Curry", "Stephen Curry", 47.94303797468354, "https://d17odppiik753x.cloudfront.net/playerimages/nba/9524.png");
+        testAthleteTwo = new Athlete(4, "Hassan", "Whiteside", "Hassan Whiteside", 35.75342465753425, "https://d17odppiik753x.cloudfront.net/playerimages/nba/12363.png");
+        testAthleteThree = new Athlete(17, "Joe", "Johnson", "Joe Johnson", 22.103703703703705, "https://d17odppiik753x.cloudfront.net/playerimages/nba/9447.png");
+        testList = new ArrayList<>();
+        testList.add(testAthleteThree);
+        testList.add(testAthleteOne);
+        testList.add(testAthleteTwo);
     }
 
     @Test
-    public void canGetAthletes() {
+    public void canGetCopyAthletes() {
         assertEquals(20, testGame.getCopyAthletes().size());
+    }
+
+    @Test
+    public void canGetQuestionAthletes() {
+        assertEquals(3, testGame.getQuestionAthletes(3).size());
+    }
+
+    @Test
+    public void canOrderAthletesByPoints() {
+        testGame.orderAthletesByPoints(testList);
+        assertEquals(3, testList.size());
+        assertEquals("Joe Johnson", testList.get(0).getFullName());
+        assertEquals("Hassan Whiteside", testList.get(1).getFullName());
+        assertEquals("Stephen Curry", testList.get(2).getFullName());
     }
 }
